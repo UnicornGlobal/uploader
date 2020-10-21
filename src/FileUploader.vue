b674c4344a216b9ebda778558a660d18a03837c5
vinothc <vinothc@techaffinity.com>
2020-10-21 23:37:57 +0530

    changes

--- src/FileUploader.vue
+++ src/FileUploader.vue
@@ -278,6 +278,7 @@
                 this.$refs['input'].click()
             },
             onChange(event) {
+                this.$emit('selected', 1);
                 return this.$validator.validateAll().then((valid) => {
                     if (!valid) {
                         this.uploading = false
